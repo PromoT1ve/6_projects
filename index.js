786ad47b1bb118fe83c701860c7c73a18ffc11ea
PromoT1ve <altynbek71822@gmail.com>
2023-02-21 17:24:34 +0600

    add_modal_window

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-    let counter = document.querySelector('h1'),
-        minusBtn = document.querySelector('.minus'),
-        plusBtn = document.querySelector('.plus');
-
-    function changeCounter() {
-        let cValue = 0;
-
-        minusBtn.addEventListener('click', () => {
-            cValue--;
-            counter.innerHTML = cValue;
-        })
-
-        plusBtn.addEventListener('click', () => {
-            cValue++;
-            counter.innerHTML = cValue;
-        })
-    }
-    
-    changeCounter();
-})

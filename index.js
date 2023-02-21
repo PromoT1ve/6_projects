4d462ca6a603ce6d38d1dd4dffa25fdcdb86b72c
PromoT1ve <altynbek71822@gmail.com>
2023-02-21 17:44:58 +0600

    modal_completed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', () => {
+    const openModalBtn = document.querySelector('.open-modal-btn'),
+          overlay = document.querySelector('.overlay'),
+          closeModalBtn = overlay.querySelector('svg');
+
+    openModalBtn.addEventListener('click', () => {
+        overlay.style.display = 'block';
+    })
+
+    closeModalBtn.addEventListener('click', () => {
+        overlay.style.display = 'none';
+    })
+
+});
+
+
